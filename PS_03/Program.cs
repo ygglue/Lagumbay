f524b532bfe99a9df5569ba14a94bf698685b44b
Eliyahu <lagumbayeliyahu@gmail.com>
2024-09-19 16:30:09 +0800

    finished assignments

---
+++ PS_03/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PS_03_Area
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Triangle_Area();
+        }
+
+        public static void Triangle_Area(double a = 10, double b = 10, double c = 10)
+        {
+            double s = (a+b+c)/2;
+            double area = Math.Sqrt(s*((s - a)*(s - b)*(s - c)));
+            Console.WriteLine(area);
+        }
+    }
+}
---
+++ PS_03_LeapYear/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PS_03_Area
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Leap_Year();
+        }
+
+        public static void Leap_Year()
+        {
+            int year = 2025;
+
+            if ((year % 4 == 0) && !(year % 100 == 0) && !(year % 400 == 0)){
+                Console.WriteLine($"{year} is a leap year");
+            }else{
+                Console.WriteLine($"{year} is not a leap year");
+            }
+        }
+    }
+}
